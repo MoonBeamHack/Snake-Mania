99f7af184a46b010c54010d0a02d0356fae0a033
MananTgs <103575280+manantgs@users.noreply.github.com>
2022-09-23 14:32:29 +0530

    sprite changes

--- Snack Mania/Assets/Scripts/StoreUI.cs
+++ Snack Mania/Assets/Scripts/StoreUI.cs
@@ -7,12 +7,12 @@ public class StoreUI : MonoBehaviour
 {
 
     public List<GameObject> DemoSnake = new List<GameObject>();
-    public List<Color> demoColor = new List<Color>();
+    public List<Sprite> demoColor = new List<Sprite>();
     public void SkinDemo(int newColor)
     {
         foreach (var item in DemoSnake)
         {
-            item.GetComponent<Image>().color = demoColor[newColor];
+            item.GetComponent<Image>().sprite = demoColor[newColor];
         }
     }
 }
